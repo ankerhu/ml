fb0fd035c11a35f1b7494ca578924ad84df4678d
ankerhu <ankerhu@foxmail.com>
2017-05-27 02:43:07 +0800

    back from guangzhou

--- bayes.py
+++ bayes.py
@@ -1,4 +1,5 @@
 from numpy import *
+#生成测试数据
 def loadDataSet():
 	postingList=[['my','dog','has','flea','problems','help','please'],
 				['maybe','not','take','him','to','dog','park','stupid'],
@@ -8,11 +9,15 @@ def loadDataSet():
 				['quit','buying','worthless','dog','food','stupid']]
 	classVec=[0,1,0,1,0,1]
 	return postingList,classVec
+
+#格式化数据为列表
 def createVocabList(dataSet):
 	vacabSet=set([])
 	for document in dataSet:
 		vacabSet=vacabSet | set(document)
 	return list(vacabSet)
+
+#转换数据为向量，出现了就是1，没出现就是0
 def setOfWords2Vec(vocabList,inputSet):
 	returnVec=[0]*len(vocabList)
 	for word in inputSet:
@@ -21,11 +26,12 @@ def setOfWords2Vec(vocabList,inputSet):
 		else:
 			print('the word:%s is not in my Vocabulary!'%word)
 	return returnVec
+
+#用已有数据训练，找到所有词的概率
 def trainNB0(trainMatrix,trainCategory):
 	numTrainDocs=len(trainMatrix)
-	
 	numWords=len(trainMatrix[0])
-
+	print(numWords)
 	pAbusive=sum(trainCategory)/float(numTrainDocs)
 	p0Num=ones(numWords)
 	p1Num=ones(numWords)
@@ -41,6 +47,7 @@ def trainNB0(trainMatrix,trainCategory):
 	p1Vect=log(p1Num/p1Denom)
 	p0Vect=log(p0Num/p0Denom)
 	return p0Vect,p1Vect,pAbusive
+
 def classifyNB(vec2Classify,p0Vect,p1Vect,pClass1):
 	p1=sum(vec2Classify*p1Vect)+log(pClass1)
 	p0=sum(vec2Classify*p0Vect)+log(1.0-pClass1)
@@ -56,7 +63,7 @@ def testingNB():
 		trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
 	p0V,p1V,pAb=trainNB0(array(trainMat),array(listClasses))
 	testEntry=['love','my','dalmation']
-	thisDoc=array(setOfWords2Vec(myVocabList,testEntry))
+	thisDoc=array( (myVocabList,testEntry))
 	print(testEntry,'classifyed as: ',classifyNB(thisDoc,p0V,p1V,pAb))
 	testEntry=['stupid','garbage']
 	thisDoc=array(setOfWords2Vec(myVocabList,testEntry))
--- testBayes.py
+++ testBayes.py
@@ -1,3 +1,12 @@
 from numpy import *
 import bayes
-bayes.testingNB()
+listOPosts,listClasses=bayes.loadDataSet()
+myVocalList=bayes.createVocabList(listOPosts)
+trainMat=[]
+for postinDoc in listOPosts:
+	trainMat.append(bayes.setOfWords2Vec(myVocalList,postinDoc))
+print(trainMat)
+p0v,p1v,pab=bayes.trainNB0(trainMat,listClasses)
+print(p0v)
+print(p1v)
+print(pab)
